8bb6b1d84f1a30c0f15de427b8712fb8640b10da
Ben Teusch <bteusch@Bens-MacBook-Air.local>
2016-09-02 16:09:17 -0600

    first recruitment analysis

---
+++ analyses/analyze_recruitment_data.R
@@ -0,0 +1,29 @@
+####
+# -- Analysis --
+####
+
+
+
+### Just start analyzing this - see how far you are from something good before you keep tweaking it forever
+### --------------
+library(psych)
+attach(wat)
+
+# start by looking at the data
+summary(wat)
+
+# question is about hiring channel
+summary(HireSource)
+
+
+### how to say it's better?
+# attrition
+describeBy(Attrition, HireSource, mat = TRUE)                                  # 
+t.test(Attrition[HireSource == "Campus"], Attrition[HireSource == "Referral"]) #
+att_mod <- lm(Attrition ~ HireSource, data = wat)
+
+# performance rating
+describeBy(PerformanceRating, HireSource, mat = TRUE)
+describeBy(SalesRating, HireSource, mat = TRUE)
+t.test(SalesRating[HireSource == "Campus"], SalesRating[HireSource == "Referral"]) #
+
--- datasets/create_recruitment_data.R
+++ datasets/create_recruitment_data.R
@@ -77,33 +77,4 @@ Education	1 'Below College'
 
 write.csv(wat, "~/hr_data/datasets/recruitment_evaluation_data.csv")
 
-####
-# -- Analysis --
-####
-
-
-
-### Just start analyzing this - see how far you are from something good before you keep tweaking it forever
-### --------------
-library(psych)
-attach(wat)
-
-# start by looking at the data
-summary(wat)
-
-# question is about hiring channel
-summary(HireSource)
-
-
-### how to say it's better?
-# attrition
-describeBy(Attrition, HireSource, mat = TRUE)                                  # 
-t.test(Attrition[HireSource == "Campus"], Attrition[HireSource == "Referral"]) #
-att_mod <- lm(Attrition ~ HireSource, data = wat)
-
-# performance rating
-describeBy(PerformanceRating, HireSource, mat = TRUE)
-describeBy(SalesRating, HireSource, mat = TRUE)
-t.test(SalesRating[HireSource == "Campus"], SalesRating[HireSource == "Referral"]) #
-
 
