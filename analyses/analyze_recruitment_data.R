4becb7066ffbbc6fae6335d5c814b9dc2ede1b74
Ben Teusch <bteusch@ip-192-168-1-7.us-west-1.compute.internal>
2016-09-06 16:06:45 -0600

    moved recruitment script to a Markdown doc

--- analyses/analyze_recruitment_data.R
+++ analyses/analyze_recruitment_data.R
@@ -2,15 +2,14 @@
 # -- Analysis --
 ####
 
-wat <- read.csv("https://raw.githubusercontent.com/teuschb/hr_data/master/datasets/recruitment_evaluation_data.csv")
+mydata <- read.csv("https://raw.githubusercontent.com/teuschb/hr_data/master/datasets/recruitment_evaluation_data.csv")
 
 ### Just start analyzing this - see how far you are from something good before you keep tweaking it forever
 ### --------------
 library(psych)
-attach(wat)
 
 # start by looking at the data
-summary(wat)
+summary(mydata)
 
 # question is about hiring channel
 summary(HireSource)
@@ -18,12 +17,12 @@ summary(HireSource)
 
 ### how to say it's better?
 # attrition
-describeBy(Attrition, HireSource, mat = TRUE)                                  # 
-t.test(Attrition[HireSource == "Campus"], Attrition[HireSource == "Referral"]) #
-att_mod <- lm(Attrition ~ HireSource, data = wat)
+describeBy(mydata$Attrition, mydata$HireSource, mat = TRUE)                                  # 
+t.test(mydata$Attrition[mydata$HireSource == "Campus"], mydata$Attrition[mydata$HireSource == "Referral"]) #
+att_mod <- lm(Attrition ~ HireSource, data = mydata)
 
 # performance rating
-describeBy(PerformanceRating, HireSource, mat = TRUE)
-describeBy(SalesRating, HireSource, mat = TRUE)
-t.test(SalesRating[HireSource == "Campus"], SalesRating[HireSource == "Referral"]) #
+describeBy(mydata$PerformanceRating, mydata$HireSource, mat = TRUE)
+describeBy(mydata$SalesRating, mydata$HireSource, mat = TRUE)
+t.test(mydata$SalesRating[mydata$HireSource == "Campus"], mydata$SalesRating[mydata$HireSource == "Referral"]) #
 
